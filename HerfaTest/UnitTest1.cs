ee6ad010e795f1a72cdab42a18c168522d33b731
Bayan Al Hassoun <B.AlHassoun.ext@Tahaluf.ae>
2024-09-19 12:49:39 +0300

    m

--- HerfaTest/UnitTest1.cs
+++ HerfaTest/UnitTest1.cs
@@ -1,87 +1,14 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using System;
+using HerfaTest.Helpers;
+using HerfaTest.POM;
 
 namespace HerfaTest
 {
     [TestClass]
     public class UnitTest1
     {
-
-
-
-        [TestMethod]
-        public void TestSuccessRegister()
-        {
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://localhost:44349/Auth/RegisterUser");
-            By firstName = By.XPath("//div/input[@id='Fname']");
-            By lastName = By.XPath("//div/input[@id='Lname']");
-            By male = By.XPath("//div/input[@value='M']");
-            By Female = By.XPath("//div/input[@value='F']");
-            By birthdate = By.XPath("//div/input[@id='Dateofbirth']");
-            By phonenumber = By.XPath("//div/input[@name='Phonenumber']");
-            By email = By.XPath("//div/input[@name='Email']");
-            By image = By.XPath("//div/input[@name='ImageFileUser']");
-            By password = By.XPath("//div/input[@id='myPass']");
-            By confirmPassword = By.XPath("//div/input[@id='myPass2']");
-            By SubmitButton = By.XPath("//div/button[@type='submit']");
-            Thread.Sleep(5000);
-            driver.FindElement(firstName).SendKeys("Raghad");
-            driver.FindElement(lastName).SendKeys("Mohammad");
-            driver.FindElement(Female).Click();
-            driver.FindElement(birthdate).SendKeys("03-03-2000");
-            driver.FindElement(phonenumber).SendKeys("07935373423");
-            driver.FindElement(email).SendKeys("Raghad1@gmail.com");
-            driver.FindElement(image).SendKeys("C:\\Users\\b.alhassoun.ext\\Downloads\\blob.jpg");
-            driver.FindElement(password).SendKeys("123456");
-            driver.FindElement(confirmPassword).SendKeys("123456");
-            driver.FindElement(SubmitButton).Click();
-            Thread.Sleep(2000);
-            var expectedURL = "https://localhost:44349/Auth/Login";
-            var actualURL = driver.Url;
-            Assert.AreEqual(expectedURL, actualURL, "Actual URL not equal the expected URL");
-            Console.WriteLine("Test Completed Successfully");
-            driver.Quit();
-        }
-
-
-        [TestMethod]
-        public void TestFailedRegister_invalidEmail()
-        {
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://localhost:44349/Auth/RegisterUser");
-            By firstName = By.XPath("//div/input[@id='Fname']");
-            By lastName = By.XPath("//div/input[@id='Lname']");
-            By male = By.XPath("//div/input[@value='M']");
-            By Female = By.XPath("//div/input[@value='F']");
-            By birthdate = By.XPath("//div/input[@id='Dateofbirth']");
-            By phonenumber = By.XPath("//div/input[@name='Phonenumber']");
-            By email = By.XPath("//div/input[@name='Email']");
-            By image = By.XPath("//div/input[@name='ImageFileUser']");
-            By password = By.XPath("//div/input[@id='myPass']");
-            By confirmPassword = By.XPath("//div/input[@id='myPass2']");
-            By SubmitButton = By.XPath("//div/button[@type='submit']");
-            Thread.Sleep(5000);
-            driver.FindElement(firstName).SendKeys("Raghad");
-            driver.FindElement(lastName).SendKeys("Mohammad");
-            driver.FindElement(Female).Click();
-            driver.FindElement(birthdate).SendKeys("03-03-2000");
-            driver.FindElement(phonenumber).SendKeys("07935373423");
-            driver.FindElement(email).SendKeys("Raghad1gmail.com");
-            driver.FindElement(image).SendKeys("C:\\Users\\b.alhassoun.ext\\Downloads\\blob.jpg");
-            driver.FindElement(password).SendKeys("123456");
-            driver.FindElement(confirmPassword).SendKeys("123456");
-            driver.FindElement(SubmitButton).Click();
-            Thread.Sleep(2000);
-            var expectedURL = "https://localhost:44349/Auth/RegisterUser";
-            var actualURL = driver.Url;
-            Assert.AreEqual(expectedURL, actualURL, "Actual URL not equal the expected URL");
-            Console.WriteLine("Test Completed Successfully");
-            driver.Quit();
-        }
 
     }
 }
--- HerfaTest/obj/Debug/net6.0/HerfaTest.AssemblyInfo.cs
+++ HerfaTest/obj/Debug/net6.0/HerfaTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HerfaTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+213ae10afda06bd4e502d92e3fd8af2890328c0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HerfaTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HerfaTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
